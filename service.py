d2a4073fe6e13045f1ba20e8b4f47911a89425c0
mtimofeev <m.timofeev@ria.ru>
2023-08-18 17:39:51 +0300

    new url in service.py

--- service.py
+++ service.py
@@ -6,7 +6,9 @@ with open("key.txt") as f:
 urlService = "https://api.lk.cdn.megafon.ru/cdn/clients/me"
 urlServiceFeatures = "https://api.lk.cdn.megafon.ru/cdn/clients/me/features"
 urlSourceGroups = "https://api.lk.cdn.megafon.ru/cdn/source_groups"
+urlResources = "https://api.lk.cdn.megafon.ru/cdn/resources"
 headers = {'Authorization': 'APIKey ' + APIkey}
-r = requests.get(urlSourceGroups, headers=headers)
+resourceId = '/164538'
+r = requests.get(urlResources + resourceId, headers=headers)
 print ("Status code: ", r.status_code)
 print ("Content: ", r.json())
