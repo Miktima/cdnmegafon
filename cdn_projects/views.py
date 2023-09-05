aec601af05729c290d2544cdd402bed7a7bad8db
Тимофеев Михаил Константинович <m.timofeev@ria.ru>
2023-09-05 17:56:34 +0300

    cdn projects without origins

--- cdn_projects/views.py
+++ cdn_projects/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from django.conf import settings
 import json
 import requests
 
 def index(request):
-    with open("../key.txt") as f:
-        APIkey = f.read().strip()
-    with open('../urls.json') as json_file:
-        urls = json.load(json_file)
-    headers = {'Authorization': 'APIKey ' + APIkey}
-    resources = requests.get(urls['urlResources'], headers=headers)
-    return render(request, "clear_cache/results.html", context)
+    headers = {'Authorization': 'APIKey ' + settings.APIKEY}
+    resources = requests.get(settings.APIURLS['urlResources'], headers=headers)
+    status_code = resources.status_code
+    res = resources.json()
+    sources = requests.get(settings.APIURLS['urlSourceGroups'], headers=headers)
+    sour = sources.json()
+    context = {
+        "status_code": status_code,
+        "response": res,
+        "sources": sour
+    }
+    return render(request, "cdn_projects/index.html", context)
