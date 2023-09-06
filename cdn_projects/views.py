be2217e3e495b75b660b02bb9d5d8d9d791228e8
Тимофеев Михаил Константинович <m.timofeev@ria.ru>
2023-09-06 14:48:38 +0300

    dict with result

--- cdn_projects/views.py
+++ cdn_projects/views.py
@@ -5,14 +5,34 @@ import requests
 
 def index(request):
     headers = {'Authorization': 'APIKey ' + settings.APIKEY}
+    # Выборка ресурсов
     resources = requests.get(settings.APIURLS['urlResources'], headers=headers)
     status_code = resources.status_code
     res = resources.json()
+    # Выборка источников
     sources = requests.get(settings.APIURLS['urlSourceGroups'], headers=headers)
     sour = sources.json()
+    result = []
+    # заполняем результат для показа
+    for r in res:
+        resDict = {}
+        resDict["id"] = r["id"]
+        resDict["descrition"] = r["description"]
+        resDict["cname"] = r["cname"]
+        # Выбираем по номеру группы источников сами источники, чтобы потом вставить в таблицу
+        for s in sour:
+            if r["originGroup"] == s["id"]:
+                sourStr = ""
+                # Если источников несколько, записываем через точку с запятой
+                for ss in s["origins"]:
+                    if len(sourStr) > 0:
+                        sourStr += "; "
+                    sourStr += ss["source"]
+                resDict["origin"] = sourStr
+                break
+        result.append(resDict)
     context = {
         "status_code": status_code,
-        "response": res,
-        "sources": sour
+        "response": result
     }
     return render(request, "cdn_projects/index.html", context)
